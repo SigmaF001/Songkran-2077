5d2429e2168c47e00488d3cdfdc919fb30c3b9ec
SigmaF001 <139973319+SigmaF001@users.noreply.github.com>
2024-01-20 21:19:13 +0700

    Update spawn item

--- System/spawn_item.gd
+++ System/spawn_item.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
 @onready var gun_lv1 = preload("res://Asset/gun.png")
+@onready var Tomyum = preload("res://Asset/Item/Tomyum.png")
+@onready var Gun = preload("res://Asset/gun.png")
 
-var data_list = ["gun_lv1", "Tomyum"]
+var data_list = ["gun_lv1", "Tomyum", "Gun"]
 
-func _ready():
-	pass # Replace with function body.
-	
+func _on_timer_timeout():
+	start_spawn()
 
-func _process(delta):
+func start_spawn():
 	data_list.shuffle()
 	var item = data_list[0]
 	var spawn_item = item.instantiate()
-
-func _on_timer_timeout():
-	pass # Replace with function body.
